3bb6ed9e1f64674fd129030b8277cdab38947b2a
maulikarche100 <maulikarche36@gmail.com>
2025-10-02 19:23:22 +0530

    second commit

---
+++ Lec-8_ConditionsDay-2/lec_8.js
@@ -0,0 +1,14 @@
+const name1 = "Ramesh";
+const name2 = "Suresh";
+
+const marks1 = 90;
+const marks2 = 50;
+
+if(marks1 > marks2){
+    console.log(name1,"has highest marks =", marks1);
+}
+else{
+    console.log(name2,"has highest Marks =", marks2);
+}
+
+
