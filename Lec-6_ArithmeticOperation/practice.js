086adae0038bc235d0863d4a531410322f02d077
maulikarche100 <maulikarche36@gmail.com>
2025-10-06 12:29:22 +0530

    ninth   commit

--- Lec-6_ArithmeticOperation/practice.js
+++ Lec-6_ArithmeticOperation/practice.js
@@ -3,4 +3,12 @@ const a = 20;
 console.log("20 minute into second is ", a*60);
 
 // addition of multiple numbers
-let 
+let b = 40;
+let c = 30;
+let d = 60;
+let e = 90;
+let f = 80;
+let g = 0;
+console.log("addition of multiple numbers =",b+c+d+e+f+g);
+
+//find a last digit of number
