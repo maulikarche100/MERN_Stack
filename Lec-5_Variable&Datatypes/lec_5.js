e5c7773132600291b893a5fb8cb617a2a0a7cc43
maulikarche100 <maulikarche36@gmail.com>
2025-10-06 11:46:03 +0530

    eight commit

---
+++ Lec-5_Variable&Datatypes/lec_5.js
@@ -0,0 +1,80 @@
+name = "Mauli Karche";
+age = 24;
+isFollow = true;
+x = null;
+y = undefined;
+console.log(name);
+console.log(age);
+console.log(isFollow);
+console.log(x);
+console.log(y);
+
+{
+//Types of Variables:
+// let, const, var
+//var: variable can be redeclared and updated
+//let: variable can not be redeclared but can be updated.
+//const: variable cannot be redeclared and can not be updated.
+}
+
+
+
+
+//ex
+let a = 5;
+console.log(a); //output is 5
+
+// //ex
+// let b = 7;
+// let b = 5;
+// console.log(b);// syntax error
+
+//ex block scope of variable let 
+{
+    let a = 7;
+    console.log(a);
+}
+{
+    let a = 5;
+    console.log(a);
+}
+
+//const variable is th fixed values
+const PI = 3.14;
+console.log(PI);
+
+
+//Datatypes in JS
+//1. Number:
+//ex
+let pen= 3;
+let price = 30;
+console.log(pen); 
+console.log(price); 
+
+//2. String
+//ex
+let myName = " Mauli Karche";
+let clgName = "JSPM's BSIOTR, Wagholi";
+console.log(myName);
+console.log(clgName);
+
+//3. Boolean
+//ex 
+let isLike = true;
+console.log(isFollow);
+
+
+// 4. undefined
+//ex
+// let x;
+// console.log(x); //Reference error
+
+
+//5 . Null
+//ex
+
+
+
+
+
---
+++ Lec-5_Variable&Datatypes/practice.js
@@ -0,0 +1,10 @@
+//ex block scope of variable let
+{
+    let b = 9;
+    console.log(a);
+}
+{
+    let b = 7;
+    console.log(a);
+}
+
