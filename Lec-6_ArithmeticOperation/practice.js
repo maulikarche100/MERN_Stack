89bc729b2d4e160aff5fa2176878acb8dc75b13a
maulikarche100 <maulikarche36@gmail.com>
2025-10-06 12:39:37 +0530

    tenth  commit

--- Lec-6_ArithmeticOperation/practice.js
+++ Lec-6_ArithmeticOperation/practice.js
@@ -12,3 +12,21 @@ let g = 0;
 console.log("addition of multiple numbers =",b+c+d+e+f+g);
 
 //find a last digit of number
+let num = 567; 
+let lastDigit = num % 10;
+console.log("last Digit of number is",lastDigit);
+
+////find a last digit of number
+let num1 = 567; 
+let num2 = num1/10;
+let x = Math.floor(num2);
+let secondLastDigit = x % 10;
+console.log("Second last Digit of number is",secondLastDigit);
+
+////find a last digit of number
+let num3 = 567; 
+let num4 = num3/100;
+let y = Math.floor(num4);
+console.log("Third last Digit of number is",y);
+
+
