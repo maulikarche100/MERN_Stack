f76bb83f9d03ca4651d23cf229f157356c640f9c
maulikarche100 <maulikarche36@gmail.com>
2025-10-06 12:08:19 +0530

    ninth commit

---
+++ Lec-6_ArithmeticOperation/lec_6.js
@@ -0,0 +1,40 @@
+//Arithmetic Operators
+// Arithmetic means calculations in maths this very important part in JS
+
+//Sum of two numbers
+//ex
+
+let a = 20;
+let b = 30;
+console.log(a+b);
+
+let c = 20;
+let d = 30;
+let e = a+b;
+console.log("Sum of two numbers=",a+b);
+
+//Subtraction of two numbers 
+//ex
+
+let x = 40;
+let y = 30;
+console.log("subtraction of two number is ", x-y);
+
+//Multiplication of two numbers 
+// ex 
+
+let f = 390;
+let g = 28;
+console.log("Multiplication of two numbers =",+ f*g);
+
+
+//Division of two Numbers
+
+//ex
+
+let h = 45;
+let i = 2;
+console.log("Division of two numbers = ", + h/i);
+
+
+
