71e99ef7838d514f2b501d988ed2be1d34ea2fbb
maulikarche100 <maulikarche36@gmail.com>
2025-10-07 09:51:27 +0530

    thirteen commit

---
+++ Lec-7_ConditionsDay-1/lec_7.js
@@ -0,0 +1,23 @@
+//New Topic----------
+//------------------------Conditional Statements--------------------
+// there is conditions like (if) or (if-else)
+let a = "12";
+let b = 12;
+if ( a == b){
+    console.log("Both a & b are same values");
+}else{
+    console.log("Both a & b are not same values");
+}
+
+//lets seee about the get operator with conditions
+//(AND, OR , NOT    gate operator)
+
+let p = 13;
+let q = 14;
+let r = 13;
+if(p == q || q == r || p == r){
+    console.log("these are the equal values")
+
+}else{
+    console.log("these are not equal value")
+}
---
+++ Lec-7_ConditionsDay-1/practice.js
@@ -0,0 +1,19 @@
+//------------------------Conditional Statements--------------------
+let a = "16";
+let b = 16;
+if ( a == b){
+    console.log("Both a & b are same values");
+}else{
+    console.log("Both a & b are not same values");
+}
+
+
+let p = 19;
+let q = 18;
+let r = 19;
+if(p == q || q == r || p == r){
+    console.log("these are the equal values")
+
+}else{
+    console.log("these are not equal value")
+}
