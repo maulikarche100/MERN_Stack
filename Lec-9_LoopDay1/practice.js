8859008a78640c3d9fe7e3054390f2a33ab14c91
maulikarche100 <maulikarche36@gmail.com>
2025-10-08 12:40:22 +0530

    19 commit

---
+++ Lec-9_LoopDay1/practice.js
@@ -0,0 +1,33 @@
+//------------loop-----------
+//For loop from 1-5
+// for( let count = 1;count <= 10; count++){
+//     console.log("mauli");
+// }
+// console.log("loop ended");
+
+
+// //You are given a number 12. Print sum of all even numbers from 1 to 12.
+// let sum = 0;
+// for (let i = 1; i <= 12; i++) {
+//     if (i % 2 === 0) {
+//         sum += i;
+//     }
+// }
+// console.log(sum);
+
+// //sum of 1 to 5
+// let sum1 = 0;
+// for(let j = 1 ; j <= 5; j++){
+//     sum1 = sum1 + j;
+// }
+// console.log(sum1);
+
+//sum of odd number from 1-12
+let sum = 0;
+for(let i = 1; i<=12;i++){
+    if(i % 2 != 0){
+        sum += i
+    }
+}
+console.log(sum);
+
