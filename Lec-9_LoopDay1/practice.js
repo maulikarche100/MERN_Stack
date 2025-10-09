5f895df0f0ba3d9dde40aa27872024a80e15489f
maulikarche100 <maulikarche36@gmail.com>
2025-10-09 11:31:48 +0530

    20   commit

--- Lec-9_LoopDay1/practice.js
+++ Lec-9_LoopDay1/practice.js
@@ -22,7 +22,7 @@ for(let j = 1 ; j <= 5; j++){
 }
 console.log(sum1);
 
-// sum of odd number from 1-12
+//ex) sum of odd number from 1-12
 let sum2 = 0;
 for(let i = 1; i<=12;i++){
     if(i % 2 != 0){
@@ -32,8 +32,54 @@ for(let i = 1; i<=12;i++){
 console.log(sum2);
 
 //print 1-10
-for (let i = 1; i <= 10; i++) {
-    console.log(i);
-}
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+//ex)
+let n = 10;
+let sum9 = 0;
+    for (let i = 1; i <= n; i++) {
+        if (i % 2 === 0) {
+            sum9 += i;
+        }
+    }
+    console.log(sum9);
+
+
+    //-------------------------while loop-----------------------------
+//ex)
+    let p = 1;
+    while(p<=5 ){
+        console.log("p=",p);
+        p++;
+    }
+
+//ex)
+    let i = 1;
+    let count = 1;
+    while(i<=5 ){
+        if(i % 2 == 0){
+            count = count+i; 
+        }
+        i++;
+    }
+    console.log(count);
+
+//--------------->>>>>>>>>>>>>> do-While ---------->>>>>>>>>
+//ex)
+let b = 1; 
+do{
+    console.log("mauli");
+    b++;
+}while(b<=20);
+
+let q = 1;
+
+
+
+
+
+
 
 
