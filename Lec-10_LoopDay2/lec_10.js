38b767fc01846cad5572e7134b1992d5f0339b31
maulikarche100 <maulikarche36@gmail.com>
2025-10-10 15:54:42 +0530

    31 commit

---
+++ Lec-10_LoopDay2/lec_10.js
@@ -0,0 +1,18 @@
+//print to find the factorial of N
+const N = 5;
+let factorial = 1;
+for(let i=1; i<=N; i++){
+    factorial *= i;
+}
+console.log(factorial);
+
+//check the prime number
+const n = 9;
+let primeNumber= 0;
+for(let i=1; i<=n; i++){
+    if(n%i==0)
+    primeNumber++;
+}
+const message = primeNumber > 2 ? "Non- Prime Number" : "Prime Number";
+console.log( n +" is a "+ message);
+
