e3897a216094873a8cebf4debf051f810b8e5515
maulikarche100 <maulikarche36@gmail.com>
2025-10-10 12:48:34 +0530

    29 commit

--- Lec-9_LoopDay1/lec_9.js
+++ Lec-9_LoopDay1/lec_9.js
@@ -91,3 +91,65 @@ for(let i = 3 ; i <= 50; i+=3){
     console.log(i);
 }
 
+
+//print the squares of each number from 20 to 35
+console.log("square of each number from 20 - 35:");
+for(let i = 20; i<=35; i++){
+    console.log(i**2);
+}
+
+
+//method -2
+// let p =35;
+// for(i=20; i<=p; i++){
+//     console.log(i**2);
+// }
+
+
+
+/*
+
+
+n =15;
+let start = 20;
+
+for(let i = 20; i <=35; i++)
+console.log(i*i);
+
+for(let i = start; i<= start + n; i++){
+    console.log(i*i);
+    //console.log(i**2);
+}
+
+*/
+
+
+// //TC -> 0(n) ----> [(start + n) - start]
+
+
+
+//print table of 5 
+// 5*1 = 5
+//5*2 = 10
+//5*3 = 15.....
+
+num = 5;
+for(let i = 1; i<=10; i++){
+    console.log(num +" * "+ i ,"=",num*i);
+}
+
+
+//find the sum of first 100 numbers and print it
+
+console.log("Sum of all numbers from 1 - 100")
+let sum = 0;
+for(i = 1; i<=100; i++){
+    sum = sum + i
+}
+console.log(sum);
+
+
+
+
+
+
