455ca32d7d2045012259a4780eeab45bb1fe2a17
maulikarche100 <maulikarche36@gmail.com>
2026-02-26 14:23:08 +0530

    loop commit

--- Lec-10_LoopDay2/lec_10.js
+++ Lec-10_LoopDay2/lec_10.js
@@ -1,18 +1,30 @@
-//print to find the factorial of N
-const N = 5;
+
+//program to find the factorial of N
+let N = 5;
 let factorial = 1;
-for(let i=1; i<=N; i++){
-    factorial *= i;
+for(let i = 1; i<=N; i++){
+    factorial *= i
 }
 console.log(factorial);
 
-//check the prime number
-const n = 13;
-let primeNumber= 0;
-for(let i=1; i<=n; i++){
-    if(n%i==0)
-    primeNumber++;
-}
-const message = primeNumber > 2 ? "Non- Prime Number" : "Prime Number";
-console.log( n +" is a "+ message);
+//check N is prime or not
 
+N = 10;
+let devisorCount = 0;
+for(let i=1; i<=N; i++){
+    if(N % i == 0)
+    devisorCount++;
+}
+const message = (devisorCount > 2) ? "Non-Prime Number" : "Prime Number";
+console.log(N ,"is a",  message)
+
+// find the hcf 
+let a =  200;
+let b = 100;
+let hcf = 1;
+for(let i = 1; i<=a && i<=b; i++){
+    if(a % i === 0 && b % i === 0){
+        hcf = i;
+    }
+}
+console.log(hcf , "is a HCF of two Number");
